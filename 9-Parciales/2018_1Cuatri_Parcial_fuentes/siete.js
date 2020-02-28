fc65ea5c8f8f49265e1640f91aeaee51d5cd6c58
alumno <alumno@lab6pc00>
2020-02-27 22:00:36 -0300

    Parcial ejercicio 7

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,49 @@
 function mostrar()
 {
+    let nota; 
+    let sexo;
+    let alumnos = [];
+    let sumaNotas = 0;
+    let notaBaja = {
+        'nota': 10
+    };
+    let varones = 0;
 
+    for (let i = 0; i < 2; i++) {
+        do {
+             nota = parseInt(prompt('Ingrese la nota del alumno: '));
+            if(isNaN(nota) || nota < 0 || nota > 10){
+                alert('Ingrese una nota valida');
+            }
+        } while (isNaN(nota) || nota < 0 || nota > 10);
+
+        do {
+            sexo = prompt('Ingrese el sexo del alumno');
+            if(sexo != 'f' && sexo !='m'){
+                alert('Ingrese un sexo valido');
+                break;
+            }
+       } while (sexo != 'f' && sexo !='m');
+
+        let data = {
+            'nota': nota,
+            'sexo': sexo
+        }
+
+        alumnos.push(data);
+    }
+
+    alumnos.forEach(el => {
+        sumaNotas += el.nota;
+        if(el.nota <= notaBaja.nota){
+            notaBaja.nota = el.nota;
+            notaBaja.sexo = el.sexo;
+        }
+        if(el.nota >= 6 && el.sexo == 'm'){
+            varones++;
+        } 
+    });
+
+    alert(`Promedio de notas: ${sumaNotas / alumnos.length}\nNota y sexo mas bajo: ${notaBaja.sexo} ${notaBaja.nota}\nCantidad de varones con nota mayor a 6: ${varones}`);
 }
+
